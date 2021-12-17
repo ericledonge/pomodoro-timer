5cabc459a764bae3218ae2626cc222ccead6ce31
ericledonge <eric.ledonge@gmail.com>
2021-12-17 11:44:24 -0500

    Modular version

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,16 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 import gear from './assets/images/gear.svg';
-import {
-  extractMinutesFromRemainingTimeInSeconds,
-  extractSecondsFromRemainingTimeInSeconds,
-  toTime,
-} from './helpers/dates';
+import Timer from './components/Time';
 
 function App() {
   const [isRunning, setIsRunning] = useState(false);
   const [isFinished, setIsFinished] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
+
   const [remainingTimeInSeconds, setRemainingTimeInSeconds] = useState(900);
 
-  const [minutesDisplayed, setMinutesDisplayed] = useState(
-    toTime(extractMinutesFromRemainingTimeInSeconds(remainingTimeInSeconds)),
-  );
-  const [secondsDisplayed, setSecondsDisplayed] = useState(
-    toTime(extractSecondsFromRemainingTimeInSeconds(remainingTimeInSeconds)),
-  );
-
-  const handleMinutesChange = (minutesInput: number) => {
-    if (minutesInput < 60) {
-      setMinutesDisplayed(toTime(minutesInput));
-      setRemainingTimeInSeconds(minutesInput * 60 + parseInt(secondsDisplayed));
-    }
-  };
-
-  const handleSecondsChange = (secondsInput: number) => {
-    if (secondsInput < 60) {
-      setSecondsDisplayed(toTime(secondsInput));
-      setRemainingTimeInSeconds(parseInt(minutesDisplayed) * 60 + secondsInput);
-    }
-  };
-
+  // handle timer running
   useEffect(() => {
     let interval: number;
     if (isRunning && remainingTimeInSeconds > 0) {
@@ -46,14 +23,8 @@ function App() {
     return () => clearInterval(interval);
   }, [isRunning, remainingTimeInSeconds]);
 
+  // handle timer ending
   useEffect(() => {
-    setMinutesDisplayed(
-      toTime(extractMinutesFromRemainingTimeInSeconds(remainingTimeInSeconds)),
-    );
-    setSecondsDisplayed(
-      toTime(extractSecondsFromRemainingTimeInSeconds(remainingTimeInSeconds)),
-    );
-
     if (isRunning && remainingTimeInSeconds === 0) {
       setIsRunning(false);
       setIsFinished(true);
@@ -89,27 +60,11 @@ function App() {
       </div>
 
       <div className="timer">
-        <div className="time">
-          <div className="minutes">
-            <input
-              type="text"
-              value={minutesDisplayed}
-              onChange={(e) => handleMinutesChange(parseInt(e.target.value))}
-              disabled={!isEditMode}
-            />
-          </div>
-
-          <div className="colon">:</div>
-
-          <div className="seconds">
-            <input
-              type="text"
-              value={secondsDisplayed}
-              onChange={(e) => handleSecondsChange(parseInt(e.target.value))}
-              disabled={!isEditMode}
-            />
-          </div>
-        </div>
+        <Timer
+          remainingTimeInSeconds={remainingTimeInSeconds}
+          isEditMode={isEditMode}
+          setRemainingTimeInSeconds={setRemainingTimeInSeconds}
+        />
 
         <button onClick={handleStartStopClick} className="start">
           {isRunning ? 'paused' : 'start'}
--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,72 +1,75 @@
-// import React, { useEffect, useState } from 'react';
-//
-// import toTime from '../helpers/dates';
-//
-// type TimeProps = {
-//   remainingTimeInSeconds: number;
-//   isEditMode: boolean;
-//   handleUpdateRemainingTime: Function;
-// };
+import React, { useEffect, useState } from 'react';
 
-// const Time = ({
-//   remainingTimeInSeconds,
-//   isEditMode = false,
-//   handleUpdateRemainingTime,
-// }: TimeProps) => {
-//   const minutes = remainingTimeInSeconds / 60;
-//   const seconds = remainingTimeInSeconds % 60;
-//
-//   const [minutesDisplayed, setMinutesDisplayed] = useState(toTime(minutes));
-//   const [secondsDisplayed, setSecondsDisplayed] = useState(toTime(seconds));
+import {
+  extractMinutesFromRemainingTimeInSeconds,
+  extractSecondsFromRemainingTimeInSeconds,
+  toTime,
+} from '../helpers/dates';
 
-// useEffect(() => {
-//   setMinutesDisplayed(toTime(remainingTimeInSeconds / 60));
-//   setSecondsDisplayed(toTime(remainingTimeInSeconds % 60));
-// }, [remainingTimeInSeconds]);
+type TimerProps = {
+  remainingTimeInSeconds: number;
+  isEditMode: boolean;
+  setRemainingTimeInSeconds: Function;
+};
 
-// const handleMinutesChange = (minutesInput: any) => {
-//   if (minutesInput < 60) {
-//     setMinutesDisplayed(toTime(minutesInput));
-//     handleUpdateRemainingTime({
-//       minutes: Math.trunc(minutesInput),
-//       seconds: secondsDisplayed,
-//     });
-//   }
-// };
-//
-// const handleSecondsChange = (secondsInput: any) => {
-//   if (secondsInput < 60) {
-//     setSecondsDisplayed(toTime(secondsInput));
-//     handleUpdateRemainingTime({
-//       minutes: minutesDisplayed,
-//       seconds: Math.trunc(secondsInput),
-//     });
-//   }
-// };
+const Timer = ({
+  remainingTimeInSeconds,
+  isEditMode = false,
+  setRemainingTimeInSeconds,
+}: TimerProps) => {
+  const [minutesDisplayed, setMinutesDisplayed] = useState(
+    toTime(extractMinutesFromRemainingTimeInSeconds(remainingTimeInSeconds)),
+  );
+  const [secondsDisplayed, setSecondsDisplayed] = useState(
+    toTime(extractSecondsFromRemainingTimeInSeconds(remainingTimeInSeconds)),
+  );
 
-//   return (
-//     <div className="time">
-//       <div className="minutes">
-//         <input
-//           type="text"
-//           value={minutesDisplayed}
-//           onChange={(e) => handleMinutesChange(e.target.value)}
-//           disabled={!isEditMode}
-//         />
-//       </div>
-//
-//       <div className="colon">:</div>
-//
-//       <div className="seconds">
-//         <input
-//           type="text"
-//           value={secondsDisplayed}
-//           onChange={(e) => handleSecondsChange(e.target.value)}
-//           disabled={!isEditMode}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
+  useEffect(() => {
+    setMinutesDisplayed(
+      toTime(extractMinutesFromRemainingTimeInSeconds(remainingTimeInSeconds)),
+    );
+    setSecondsDisplayed(
+      toTime(extractSecondsFromRemainingTimeInSeconds(remainingTimeInSeconds)),
+    );
+  }, [remainingTimeInSeconds]);
 
-// export default Time;
+  const handleMinutesChange = (minutesInput: number) => {
+    if (minutesInput < 60) {
+      setMinutesDisplayed(toTime(minutesInput));
+      setRemainingTimeInSeconds(minutesInput * 60 + parseInt(secondsDisplayed));
+    }
+  };
+
+  const handleSecondsChange = (secondsInput: number) => {
+    if (secondsInput < 60) {
+      setSecondsDisplayed(toTime(secondsInput));
+      setRemainingTimeInSeconds(parseInt(minutesDisplayed) * 60 + secondsInput);
+    }
+  };
+
+  return (
+    <div className="time">
+      <div className="minutes">
+        <input
+          type="text"
+          value={minutesDisplayed}
+          onChange={(e) => handleMinutesChange(parseInt(e.target.value))}
+          disabled={!isEditMode}
+        />
+      </div>
+
+      <div className="colon">:</div>
+
+      <div className="seconds">
+        <input
+          type="text"
+          value={secondsDisplayed}
+          onChange={(e) => handleSecondsChange(parseInt(e.target.value))}
+          disabled={!isEditMode}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
